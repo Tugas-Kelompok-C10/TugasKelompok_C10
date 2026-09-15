0420d2e37fa1503bf92e1dd02afdf28c5c64c331
deepanooo <islamifxadib@gmail.com>
2026-09-15 14:48:31 +0700

    check rocket page PWS

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-!8uy@kctpc#()rp)2$z0^#0!9kf4!b!)=49%9%%ij!nmmif#4o
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = not PRODUCTION
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "muhammad-adib51-tugaskelompokc10.pws.cs.ui.ac.id"]
 
